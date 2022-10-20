5b54e7c0a902d71433948e02e7a63d7e9774d866
taka0713 <tyunisu2018@gmail.com>
2022-10-21 01:15:48 +0900

    message

--- script.js
+++ script.js
@@ -1,46 +1,29 @@
 $(function () {
   // 回した数を管理する変数
   let spin_manage = 0;
-  let init = () => {
-    // ビンゴカードの数字を初期化する処理
-    let range = [];
-    for (let i = 0; i <= 49; i++) {
-      range.push(i);
-    }
-    let bingo_card = [];
-    $.each(range, function () {
-      let random = Math.floor(Math.random() * range.length);
-      bingo_card.push(range.splice(random, 1)[0]);
-    });
-    $(".card_number").map(function (n) {
-      $(this).text(bingo_card[n]);
-      $(this).prop("id", "display_number_" + n);
-      console.log("display_number_" + n);
-    });
-    // ランダムで出た数字の色を初期化
-    for (let i = 0; i < 50; i++) {
-      $("#target_" + i).css("color", "black");
-    }
-    spin_manage = 0;
-    $("#spin_count").val(0);
-    $("#bingo_number").text("-");
-  };
-
-  init();
+  let init = () => {};
+  let random = Math.floor(Math.random() * 49) + 1;
+  $(".card_number").map(function () {
+    random = Math.floor(Math.random() * 49) + 1;
+    $(this).text(random);
+    let loop = [];
+    loop.push(random);
+    console.log(loop);
+  });
 
   $("#spin").on("click", function () {
-    let spin_random = Math.floor(Math.random() * 49);
-    $("#bingo_number").text(spin_random);
-    $("#" + "target_" + spin_random).css("color", "red");
-    $(".card_number").map(function (n) {
-      $(this).prop("id");
-    });
+    let;
   });
 
-  $("#reset").on("click", function () {
-    init();
-  });
+  $("#reset").on("click", function () {});
 });
+
+// 更新するとビンゴカードの数字が０から４９までの整数がランダムで配置される(数字が被らないように)
+// 回すを押すと０から４９までの整数がランダムで数字が表示される(数字が被らないように)
+// 回した回数をカウント
+// 出た数字を赤文字にする
+// リーチ、ビンゴの数をカウント
+// リセットボタンで全てクリア
 
 // let random = Math.floor(Math.random() * 49) + 1;
 // let spin_number = [];
@@ -53,3 +36,28 @@ $(function () {
 // $("#target_" + random).css("color", "red");
 // spin_manage++;
 // $("#spin_count").val(spin_manage);
+
+// let init = () => {
+//   // ビンゴカードの数字を初期化する処理
+//   let range = [];
+//   for (let i = 0; i <= 49; i++) {
+//     range.push(i);
+//   }
+//   let bingo_card = [];
+//   $.each(range, function () {
+//     let random = Math.floor(Math.random() * range.length);
+//     bingo_card.push(range.splice(random, 1)[0]);
+//   });
+//   $(".card_number").map(function (n) {
+//     $(this).text(bingo_card[n]);
+//     $(this).prop("id", "display_number_" + n);
+//     console.log("display_number_" + n);
+//   });
+//   // ランダムで出た数字の色を初期化
+//   for (let i = 0; i < 50; i++) {
+//     $("#target_" + i).css("color", "black");
+//   }
+//   spin_manage = 0;
+//   $("#spin_count").val(0);
+//   $("#bingo_number").text("-");
+// };
